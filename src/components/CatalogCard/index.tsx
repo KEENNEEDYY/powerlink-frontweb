ef603c8c1291d22ddb41fd62fc625d3105d72e6b
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-02-20 08:08:34 -0300

    Customer Area card
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,8 +1,28 @@
-export default function CatalogCard() {
+import './styles.css';
 
-    return(
-        <>
-            <h1>CatalogCard</h1>
-        </>
+type Props = {
+    itemName: string;
+    imgUrl: string;
+    itemDescription: string;
+    textButton: string;
+}
+
+export default function CatalogCard(
+    {itemName, imgUrl, itemDescription, textButton} : Props
+) {
+
+    return (
+        <div className="pwl-section-card">
+            <img src={imgUrl} alt="TrackingService" />
+            <h1>{itemName}</h1>
+            <p>
+                <i>
+                    {itemDescription}
+                </i>
+            </p>
+            <button className="pwl-button-primary pwl-card-button">
+                {textButton}
+            </button>
+        </div>
     )
 }
--- src/routes/CustomerArea/index.tsx
+++ src/routes/CustomerArea/index.tsx
@@ -1,4 +1,5 @@
 import CatalogCard from '../../components/CatalogCard';
+import imgUrl from '../../assets/car-tracking-card.png';
 import './styles.css';
 
 export default function CustomerArea() {
@@ -8,8 +9,16 @@ export default function CustomerArea() {
             <section id="pwl-customer-area-section">       
                 <div className="pwl-customer-area-container">
                     <div className="pwl-customer-section">
-                        <CatalogCard />
-                        <CatalogCard />
+                        <CatalogCard
+                            imgUrl={imgUrl}
+                            itemName='Plataforma de rastreamento veívular'
+                            itemDescription='
+                            Aqui você poderá ter acesso a localização do seu veículo em tempo real e usar das 
+                            diversas funções contratadas como: bloqueio/desbloqueio da ignição e muito mais.
+                            '
+                            textButton='Acessar'
+
+                         />
                     </div>
                 </div>
             </section>
