49f1b836cdbe3c830b706daa9cae5b92e0ac3bc9
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-02-25 17:30:34 -0300

    Hamburguer menu isOpen
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
 import powerlinkImg from '../../assets/Logo.svg';
-import menuImg from '../../assets/hamburger.svg';
+import menuClose from '../../assets/hamburger.svg';
+import menuOpen from '../../assets/hamburger-open.svg';
 import instagramImg from '../../assets/instagram.svg';
 import linkedinImg from '../../assets/linkedin.svg';
 
 import './styles.css';
+import { useState } from 'react';
 
 export default function Header() {
-    return(
+
+    const [active, setMode] = useState(false);
+    const ToggleMode = () => {
+        setMode(!active);
+    }
+
+    return (
         <>
             <header className="pwl-header">
                 <nav className="pwl-navbar">
@@ -15,17 +23,30 @@ export default function Header() {
                         <img className="pwl-powerlink-logo" src={powerlinkImg} alt="Logo" />
                     </a>
                     <div className="pwl-menu-itens">
-                        <img className="pwl-menu-img" src={menuImg} alt="Menu" />    
-                        <div className="pwl-menu-text">
-                            <h3 className="pwl-display-none">Home</h3>
-                            <h3 className="pwl-display-none">Sobre nós</h3>
-                            <h3 className="pwl-display-none">Catalog</h3>
-                            <a href="mailto:contato@powerlinkco.com.br" target='_blank'>
-                                <h3>Contato</h3>
-                            </a>
-                            <Link to="customerArea">
-                                <h3>Área do Cliente</h3>
-                            </Link>
+                        <div onClick={ToggleMode}>
+                            {!active &&
+                                <img className="pwl-menu-img" src={menuClose} alt="Menu" />
+                            }
+                            {active &&
+                                <>
+                                    <img className="pwl-menu-img pwl-menu-display-show" src={menuOpen} alt="Menu" />
+                                </>
+                            }
+                        </div>
+                        <div>
+                            <div className={active ? "pwl-menu-display-show" : "pwl-menu-text"}>
+                                <h3 className="pwl-display-none">Home</h3>
+                                <h3 className="pwl-display-none">Sobre nós</h3>
+                                <h3 className="pwl-display-none">Catalog</h3>
+                                <a className={active ? "pwl-menu-display-show" : "pwl-menu-hide"}
+                                    href="mailto:contato@powerlinkco.com.br" target='_blank'>
+                                    <h3>Contato</h3>
+                                </a>
+                                <Link className={active ? "pwl-menu-display-show" : "pwl-menu-hide"}
+                                    to="customerArea">
+                                    <h3>Área do Cliente</h3>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                     <div className="pwl-social-media">
@@ -34,7 +55,7 @@ export default function Header() {
                         </a>
                         <a href="https://www.linkedin.com/company/powerlink-co/" target='_blank'>
                             <img src={linkedinImg} alt="Linkedin" />
-                        </a>    
+                        </a>
                     </div>
                 </nav>
             </header>
